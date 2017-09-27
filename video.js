a21c6bd866bbd2f3be21a7187cf7b5f3b91927e7
Ignat Goldman <mail@ignat.co.uk>
2017-09-27 17:08:17 +0300

    fix: variable name

--- video.js
+++ video.js
@@ -3,7 +3,7 @@ let video = document.getElementById("bgvid");
 function vidFade() {
   video.classList.add("stopfade");
 }
-vid.addEventListener("ended", function() {
+video.addEventListener("ended", function() {
   // to capture IE10
   vidFade();
 });
